c985df9b0979baf863b59eeec0b877e4db9fdd68
Nikita Grischenko <ngrischenko@adyax.com>
2020-11-20 09:36:21 +0200

    progress bar

--- Totemplay/js/main.js
+++ Totemplay/js/main.js
@@ -4,6 +4,19 @@ function initAccordion() {
     });
 }
 
+function checkProgress() {
+    var progressBar = document.querySelector('.progress-bar');
+    var position = document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var progress = (position / height) * 100;
+    progressBar.style.width = progress + "%";
+}
+
 $(document).ready(function() {
     initAccordion();
+    checkProgress();
+});
+
+document.addEventListener('scroll', function() {
+    checkProgress();
 });
